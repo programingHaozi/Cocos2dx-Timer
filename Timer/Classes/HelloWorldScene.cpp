d6f023c8e047a5b22fecb7ba0e85ef3501934264
chenhao <229520281@qq.com>
2014-11-05 17:43:41 +0800

    Cocos2dx-Timer
    Cocos2dx-Timer

---
+++ Timer/Classes/HelloWorldScene.cpp
@@ -0,0 +1,65 @@
+#include "HelloWorldScene.h"
+#include "cocostudio/CocoStudio.h"
+#include "ui/CocosGUI.h"
+
+USING_NS_CC;
+
+using namespace cocostudio::timeline;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    label = Label::createWithSystemFont("chenhao", "Courier", 30);
+    addChild(label);
+//    scheduleUpdate();
+    
+    
+    schedule(schedule_selector(HelloWorld::timerHanler), 1.0f);
+
+    return true;
+}
+
+
+void HelloWorld::update(float delta){
+    label ->setPosition(label ->getPosition()+Point(1,1));
+}
+
+void HelloWorld::timerHanler(float dt){
+    log(">>>>>>>");
+}
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    return;
+#endif
+
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
